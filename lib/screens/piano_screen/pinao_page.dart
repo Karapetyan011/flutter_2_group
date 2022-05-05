7f774d07fb015143dcaf554a0a0e8975bb292c15
hrantkarapetyan011 <hrantkarapetyan011@gmail.com>
2022-05-05 16:34:21 +0400

    additions and updates have been made in piano page

--- lib/screens/piano_screen/pinao_page.dart
+++ lib/screens/piano_screen/pinao_page.dart
@@ -25,20 +25,22 @@ class PianoApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            buildKey(color: Colors.red, soundNumber: 1),
-            buildKey(color: Colors.orange, soundNumber: 2),
-            buildKey(color: Colors.yellow, soundNumber: 3),
-            buildKey(color: Colors.green, soundNumber: 4),
-            buildKey(color: Colors.teal, soundNumber: 5),
-            buildKey(color: Colors.blue, soundNumber: 6),
-            buildKey(color: Colors.purple, soundNumber: 7),
-          ],
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
